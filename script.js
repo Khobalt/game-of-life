fc00f5c935c351a5d7b91f4ca0095a2d01ac89b2
Lee Hardy Dydo <lee@dydo.co>
2023-05-28 15:59:35 -0400

    Fix clipping crash
    Also print some debug info and draw grid lines
    with assigned key presses.

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ let cellSize = 5;
 let playerCellSize = 5;
 let score = 0;
 
+let debug = false;
+let gridLines = false;
+
 const numRows = canvas.height / cellSize;
 const numCols = canvas.width / cellSize;
 
@@ -27,6 +30,14 @@ function createGrid(numRows, numCols) {
     return grid;
 }
 
+let player = {
+    row: 100,
+    col: 100
+};
+
+//player.col = Math.floor(Math.random() * numCols);
+//player.row = Math.floor(Math.random() * numRows);
+
 //Draw
 
 function drawGrid() {
@@ -44,7 +55,6 @@ function drawGrid() {
 function drawPlayer() {
     ctx.fillStyle = 'red';
     ctx.fillRect(player.col * cellSize, player.row * cellSize, playerCellSize, playerCellSize);
-    //ctx.fillStyle = 'blue'; // set fill style back to black for other cells
 }
 
 //Draw score
@@ -52,6 +62,53 @@ function drawScore() {
     ctx.font = '16px Arial';
     ctx.fillStyle = 'white';
     ctx.fillText('Score: ' + score, 8, 20);
+}
+
+function drawGridLines() {
+
+    ctx.strokeStyle = 'rgba(255, 255, 255, 0.3)';
+    ctx.lineWidth = 1;
+    for (let i = 0; i < numRows; i++) {
+        ctx.beginPath();
+        ctx.moveTo(0, i * cellSize);
+        ctx.lineTo(canvas.width, i * cellSize);
+        ctx.stroke();
+    }
+    for (let j = 0; j < numCols; j++) {
+        ctx.beginPath();
+        ctx.moveTo(j * cellSize, 0);
+        ctx.lineTo(j * cellSize, canvas.height);
+        ctx.stroke();
+    }
+
+}
+
+function drawDebug() {
+    ctx.font = '16px Arial';
+    ctx.fillStyle = 'white';
+    ctx.fillText('Player Row: ' + player.row, 8, 40);
+    ctx.fillText('Player Col: ' + player.col, 8, 60);
+
+    // The player's bounding box
+    ctx.fillStyle = 'green';
+    ctx.fillText('Player Top: ' + player.row * cellSize, 8, 80);
+    ctx.fillText('Player Bottom: ' + (player.row * cellSize + playerCellSize - 1), 8, 100);
+    ctx.fillText('Player Left: ' + player.col * cellSize, 8, 120);
+    ctx.fillText('Player Right: ' + (player.col * cellSize + playerCellSize - 1), 8, 140);
+
+    ctx.fillStyle = 'white';
+    ctx.fillText('Player Top Cell: ' + Math.max(0, Math.floor(player.row / cellSize)), 8, 160);
+    ctx.fillText('Player Bottom Cell: ' + Math.floor((player.row + playerCellSize - 1) / cellSize), 8, 180);
+    ctx.fillText('Player Left Cell: ' + Math.floor(player.col / cellSize), 8, 200);
+    ctx.fillText('Player Right Cell: ' + Math.floor((player.col + playerCellSize - 1) / cellSize), 8, 220);
+
+    ctx.fillText('Total Rows: ' + numRows, 8, 240);
+    ctx.fillText('Total Cols: ' + numCols, 8, 260);
+
+    ctx.fillText('Player Cell Size: ' + playerCellSize, 8, 280);
+    ctx.fillText('Cell Size: ' + cellSize, 8, 300);
+
+
 }
 
 function updateGrid() {
@@ -91,10 +148,6 @@ function countNeighbors(row, col) {
     return count;
 }
 
-let player = {
-    row: 0,
-    col: 0
-};
 
 
 document.addEventListener('keydown', function (event) {
@@ -109,6 +162,20 @@ document.addEventListener('keydown', function (event) {
     }
 });
 
+// turn on and off debug mode
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'd') {
+        debug = !debug;
+    }
+});
+
+// turn on and off grid lines
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'g') {
+        gridLines = !gridLines;
+    }
+});
+
 function detectCollisions() {
     // Calculate the player's bounding box
     const playerTop = player.row * cellSize;
@@ -116,28 +183,34 @@ function detectCollisions() {
     const playerLeft = player.col * cellSize;
     const playerRight = playerLeft + playerCellSize - 1;
 
-    // Check for collisions with all cells that the player overlaps with
-    for (let row = Math.max(0, Math.floor(playerTop / cellSize)); row <= Math.floor(playerBottom / cellSize); row++) {
-        for (let col = Math.floor(playerLeft / cellSize); col <= Math.floor(playerRight / cellSize); col++) {
-            if (grid[row][col] === 1) {
-                grid[row][col] = 0;
-                score++;
-                if (score % 10 === 0) {
-                    increasePlayerSize();
-                }
-            }
-        }
-    }
 
     // Keep the player within the canvas bounds
     if (player.row < 0) {
         player.row = 0;
-    } else if (player.row + playerCellSize > canvas.height) {
-        player.row = canvas.height - playerCellSize;
+    } else if (player.row + playerCellSize / cellSize >= numRows) {
+        player.row = numRows - playerCellSize / cellSize;
+        if (player.col + playerCellSize / cellSize >= numCols) {
+            player.col = numCols - playerCellSize / cellSize;
+        }
     } else if (player.col < 0) {
         player.col = 0;
-    } else if (player.col + playerCellSize > numCols * cellSize) {
-        player.col = numCols * cellSize - playerCellSize;
+    } else if (player.col + playerCellSize / cellSize >= numCols) {
+        player.col = numCols - playerCellSize / cellSize;
+    }
+
+    // Check for collisions with all cells that the player overlaps with
+    for (let row = Math.max(0, Math.floor(playerTop / cellSize)); row <= Math.floor(playerBottom / cellSize); row++) {
+        for (let col = Math.floor(playerLeft / cellSize); col <= Math.floor(playerRight / cellSize); col++) {
+            if (grid[row]) {
+                if (grid[row][col] === 1) {
+                    grid[row][col] = 0;
+                    score++;
+                    if (score % 100 === 0) {
+                        increasePlayerSize();
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -148,7 +221,7 @@ function spawnCells() {
     for (let i = 0; i < 4; i++) {
         let row = Math.floor(Math.random() * numRows);
         let col = Math.floor(Math.random() * numCols);
-        
+
         //if cell is already alive, try again
         if (grid[row][col] === 1) {
             i--;
@@ -163,6 +236,10 @@ document.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
         score = 0;
         grid = createGrid(numRows, numCols);
+        player.row = Math.floor(numRows / 2);
+        player.col = Math.floor(numCols / 2);
+        playerCellSize = cellSize;
+
     }
 });
 
@@ -190,21 +267,23 @@ function increasePlayerSize() {
 
 let frame = 0;
 function gameLoop() {
-    
+
     if (!paused) {
         //update the grid every 2 seconds
         //if (frame % 20 === 0) {
-            updateGrid();
-            //}
-            detectCollisions();
-            
-            //spawnCells();
-            frame++;
-            drawGrid();
-            drawPlayer();
-            drawScore();
-            
-        }
+        updateGrid();
+        //}
+        detectCollisions();
+
+        //spawnCells();
+        frame++;
+        drawGrid();
+        drawPlayer();
+        drawScore();
+        if (gridLines) drawGridLines();
+        if (debug) drawDebug();
+
+    }
     requestAnimationFrame(gameLoop);
 }
 
